2839fd3c7464a56a21e454a2ce53224cc62c2f0a
Programmerdj20 <andreschmde@gmail.com>
2023-08-18 14:33:48 -0400

    Wrapper Change

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -8,7 +8,7 @@ const Wrapper = () => {
                     <div class="px-6 text-center text-white md:px-12">
                         <h1 class="mt-6 mb-16 text-5xl font-bold tracking-tight md:text-5xl">
                             The best offer on the market <br />
-                            <span>for your business</span>
+                            <span>Para tu Negocio</span>
                         </h1>
                         <a
                             class="mb-2 inline-block rounded-full border-2 font-bold border-neutral-50 px-[46px] pt-[14px] pb-[12px] text-4xl leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 md:mr-2 md:mb-0"
